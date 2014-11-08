0f755eba03d9ad6a4ae6f3072de238ff12556f30
Nikhil Kulkarni <Nikhil896@gmail.com>
2014-11-08 10:59:02 -0600

    Added first files

---
+++ node/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('default', {
+		title: 'Home',
+		users:['Nikhil', 'app', 'random']
+		});
+});
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/me', function(req, res){
+	res.send('@nikhkulkarni');
+});
+
+app.get('/who/:name?', function(req, res) {
+	var name = req.params.name;
+	res.send(name);
+});
+
+app.get('/who/:name?/:title?', function(req, res) {
+	var name = req.params.name;
+	var title = req.params.title;
+	res.send('name: ' + name + '<br></br> title: ' + title);
+});
+
+app.get('*', function(req, res) {
+	res.send('bad route');
+});
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port 3000');
+})
