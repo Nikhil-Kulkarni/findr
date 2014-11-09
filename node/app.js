9333a0570bced82825c80357043dbc47c4376c30
Nikhil Kulkarni <Nikhil896@gmail.com>
2014-11-08 18:20:16 -0600

    Server for input is now up
    Run node app.js

--- node/app.js
+++ node/app.js
@@ -1,34 +1,24 @@
 var express = require('express');
 var app = express();
 var name = "asdf";
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views/styles'));
+app.use(bodyParser());
 
 app.get('/', function(req, res) {
 	res.render('default');
 });
 
-app.post('/nextPage', function(req, res) {
-	console.log('Location: ' + req.body.location);
-})
-
-// app.get('/list', function(req, res) {
-// 	var location = req.body.loc;
-// 	var cuisine = req.body.cuisine;
-// 	var restriction = req.body.restriction;
-// 	console.log(location + cuisine + restriction);
-// 	res.render('list');
-// })
-
 app.post('/list', function(req, res) {
-	var location = req.body.loc;
-	var cuisine = req.body.cuisine;
-	var restriction = req.body.restriction;
-	console.log(location + cuisine + restriction);
+	console.log(req.body.loc);
+	console.log(req.body.cuisine);
+	console.log(req.body.restrictions);
 	res.render('list');
 })
 
+/* TESTING CODE BELOW ------>  CODE ABOVE*/
 app.get('/who/:name?/:title?', function(req, res) {
 	var name = req.params.name;
 	var title = req.params.title;
